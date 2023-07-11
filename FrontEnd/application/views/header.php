f38b293c0aa93ba55e2a5c9f76ed7a36c890e380
Jeremie04 <rantojeremie@gmail.com>
2023-07-11 13:42:06 -0700

    update_assets

--- FrontEnd/application/views/header.php
+++ FrontEnd/application/views/header.php
@@ -50,7 +50,7 @@
               <!-- Sidebar user (optional) -->
             <div class="user-panel mt-3 pb-3 mb-3 d-flex">
                 <div class="image">
-                <img src="../../dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+                <img src="<?php echo site_url('assets/image/user/avatar.png') ?>" class="img-circle elevation-2" alt="User Image">
                 </div>
                 <div class="info">
                 <a href="#" class="d-block"><?php echo $user['nom'] ?></a>
